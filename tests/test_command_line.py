e94a50118de57aa4262c63457f399494f665fe3a
Erik Hauck <erikmhauck@gmail.com>
2018-02-27 22:09:32 -0800

    added command_line.py tests

---
+++ tests/test_command_line.py
@@ -0,0 +1,17 @@
+from converto.command_line import parse_args, setup_logging
+import logging
+
+
+def test_parse_args():
+    # act
+    args = parse_args()
+    # assert
+    assert args.config == None
+    assert args.debug == False
+    assert args.input == None
+
+def test_setup_logging():
+    # act
+    setup_logging(False)
+    # assert
+    assert True
